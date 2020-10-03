a101145149f04029134346a3621807b09fe0bcf1
Ash <asherchez@gmail.com>
2020-10-03 19:59:07 +0100

    Updates

---
+++ precache-manifest.02d81bc9887391496c380da9b8af6cb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dd5916fed162c4dd4b07630b330a2dd",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "5360efcf381a76aea2df",
+    "url": "/todolist/static/css/main.c6977ecb.chunk.css"
+  },
+  {
+    "revision": "65cb2e9f31e48650928f",
+    "url": "/todolist/static/js/2.f158a218.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist/static/js/2.f158a218.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5360efcf381a76aea2df",
+    "url": "/todolist/static/js/main.66e5221f.chunk.js"
+  },
+  {
+    "revision": "197f51c8578d731c6831",
+    "url": "/todolist/static/js/runtime-main.e7725399.js"
+  }
+]);
